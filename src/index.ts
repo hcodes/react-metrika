9bf678e1f8080e2b1d771c63481c264996c7bd41
Denis Seleznev <hcodes@users.noreply.github.com>
2024-09-06 00:06:50 +0300

    Add additional import

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './consts';
 
 export { loadMetrikaScript, isMetrikaScriptLoaded } from './utils/counter';
 export { sendAction as ym, setMetrikaScriptUrl } from './sendAction';
+export { clearDelayedBuffer } from './utils/delayedActionBuffer';
