e9a2dcdd4096c060c35cb0ae9d66f8f96cfc462e
Leonid Fedorov <lfedorov@live.com>
2026-08-19 18:09:55 +0300

    Add scriptUrl prop to Metrika components

--- src/components.ts
+++ src/components.ts
@@ -1,13 +1,18 @@
-import { sendAction } from './sendAction';
+import { sendAction, setMetrikaScriptUrl } from './sendAction';
 
 export interface MetrikaCounterProps {
     id: number;
     options?: YaMetrika2Options;
+    scriptUrl?: string;
 }
 
 export function MetrikaCounter(props: MetrikaCounterProps): null {
     if (typeof window === 'undefined') {
         return null;
+    }
+
+    if (typeof props.scriptUrl !== 'undefined') {
+        setMetrikaScriptUrl(props.scriptUrl);
     }
 
     sendAction(props.id, 'init', props.options || {});
@@ -18,11 +23,16 @@ export function MetrikaCounter(props: MetrikaCounterProps): null {
 export interface MetrikaCountersProps {
     ids: number[];
     options?: YaMetrika2Options;
+    scriptUrl?: string;
 }
 
 export function MetrikaCounters(props: MetrikaCountersProps): null {
     if (typeof window === 'undefined') {
         return null;
+    }
+
+    if (typeof props.scriptUrl !== 'undefined') {
+        setMetrikaScriptUrl(props.scriptUrl);
     }
 
     props.ids.forEach(id => sendAction(id, 'init', props.options || {}));
